d1c5231e3f30df68f70c81b02e028c09eb1a58d3
unknown <treykadenyi@gmail.com>
2022-09-14 15:53:44 +0300

    Create two new tables: owners and species and update animals table to remove the species column

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-
+-- animals table
 CREATE TABLE animals (
   id INT NOT NULL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
@@ -9,3 +9,50 @@ CREATE TABLE animals (
   weight_kg NUMERIC(5, 2) NOT NULL,
   species VARCHAR(50)
 );
+
+-- owners table
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  
+  full_name VARCHAR(50) NOT NULL,
+  age INT
+);
+
+-- species table
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+
+--  new animals
+CREATE TABLE new_animals (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg NUMERIC(5, 2) NOT NULL,
+  species VARCHAR(50)
+);
+-- copy data from animals to new_animals except for id
+INSERT INTO new_animals(name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+SELECT name, date_of_birth, escape_attempts, neutered, weight_kg, species FROM animals;
+
+-- drop animals table
+DROP TABLE animals;
+-- rename new_animals to animals
+ALTER TABLE new_animals
+RENAME TO animals;
+
+-- remove column species from animals
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id)
+ON DELETE CASCADE;
+
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
