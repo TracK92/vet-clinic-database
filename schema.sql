7e0194c5d8f6a0dac7ffb6f7a9db7ad887339595
unknown <treykadenyi@gmail.com>
2022-09-14 20:42:38 +0300

    Add new tables to the database

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE animals (
 
 -- owners table
 CREATE TABLE owners (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   full_name VARCHAR(50) NOT NULL,
   age INT
 );
@@ -22,7 +22,6 @@ CREATE TABLE species (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(50) NOT NULL
 );
-
 
 --  new animals
 CREATE TABLE new_animals (
@@ -34,25 +33,70 @@ CREATE TABLE new_animals (
   weight_kg NUMERIC(5, 2) NOT NULL,
   species VARCHAR(50)
 );
+
 -- copy data from animals to new_animals except for id
-INSERT INTO new_animals(name, date_of_birth, escape_attempts, neutered, weight_kg, species)
-SELECT name, date_of_birth, escape_attempts, neutered, weight_kg, species FROM animals;
+INSERT INTO
+  new_animals(
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg,
+    species
+  )
+SELECT
+  name,
+  date_of_birth,
+  escape_attempts,
+  neutered,
+  weight_kg,
+  species
+FROM
+  animals;
 
 -- drop animals table
 DROP TABLE animals;
+
 -- rename new_animals to animals
-ALTER TABLE new_animals
-RENAME TO animals;
+ALTER TABLE
+  new_animals RENAME TO animals;
 
 -- remove column species from animals
-ALTER TABLE animals
-DROP COLUMN species;
+ALTER TABLE
+  animals DROP COLUMN species;
 
 -- Add column species_id which is a foreign key referencing species table
-ALTER TABLE animals
-ADD COLUMN species_id INT REFERENCES species(id)
-ON DELETE CASCADE;
+ALTER TABLE
+  animals
+ADD
+  COLUMN species_id INT REFERENCES species(id) ON DELETE CASCADE;
 
 -- Add column owner_id which is a foreign key referencing the owners table
-ALTER TABLE animals
-ADD COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
+ALTER TABLE
+  animals
+ADD
+  COLUMN owner_id INT REFERENCES owners(id) ON DELETE CASCADE;
+
+-- JOIN TABLE FOR VISITS
+-- create table named vets
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(50),
+  age INT,
+  date_of_graduation DATE
+);
+
+-- join table called specialization to handle relationship between species and vets
+CREATE TABLE specialization (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  species_id INT REFERENCES species(id) ON DELETE CASCADE,
+  vet_id INT REFERENCES vets(id) ON DELETE CASCADE
+);
+
+-- join table called visits to handle relationship between animals and vets
+CREATE TABLE visits (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  animal_id INT REFERENCES animals(id) ON DELETE CASCADE,
+  vet_id INT REFERENCES vets(id) ON DELETE CASCADE,
+  date_of_visit DATE NOT NULL
+);
