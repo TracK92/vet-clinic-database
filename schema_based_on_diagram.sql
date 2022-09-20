4b2f4625e7e1b40e35b4de78365373f2b9c8f95c
unknown <treykadenyi@gmail.com>
2022-09-20 14:16:01 +0300

    Generate four tables from the given diagram

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,30 @@
+-- patients table
+CREATE TABLE patients (
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE
+);
+
+-- medical histories table
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT NOT NULL,
+  status VARCHAR(20) NOT NULL
+);
+
+-- treatments table
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  type VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL
+);
+
+-- invoices table
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  total_amount DECIMAL(10,2) NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP,
+  medical_history_id INT
+)
